0e4887742d82887c51d40ff81e2b62e5fb6e1edb
Kelsey Benedict <kelseybenedict26@gmail.com>
2021-05-30 01:22:01 -0600

    added put route for workout by ID

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,37 +5,50 @@ const Workout = require('../models/workout.js');
 // route for creating new workout -- POST
 router.post('/api/workouts', (req, res) => {
     Workout.create({})
-      .then((workouts) => {
-        res.json(workouts);
-      })
-      // error handlinga
-      .catch((err) => {
-        res.json(err);
-      });
-  });
+        .then((workouts) => {
+            res.json(workouts);
+        })
+        // error handlinga
+        .catch((err) => {
+            res.json(err);
+        });
+});
 
 // route for getting all workouts with their duration -- GET
 router.get('/api/workouts', (req, res) => {
     // referenced https://docs.mongodb.com/manual/aggregation/
     Workout.aggregate([
-      {
-        // adding the field to include the duration of the exercise
-        $addFields: {
-          totalDuration: {
-            $sum: '$exercises.duration',
-          },
+        {
+            // adding the field to include the duration of the exercise
+            $addFields: {
+                totalDuration: {
+                    $sum: '$exercises.duration',
+                },
+            },
         },
-      },
     ])
-      .then((workouts) => {
-        res.json(workouts);
-      })
-      // error handling
-      .catch((err) => {
-        res.json(err);
-      });
-  });
+        .then((workouts) => {
+            res.json(workouts);
+        })
+        // error handling
+        .catch((err) => {
+            res.json(err);
+        });
+});
 // route for updating workout by ID -- PUT
-
+router.put('/api/workouts/:id', ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+        params.id,
+        { $push: { exercises: body } },
+        { new: true, runValidators: true } // checking to see if new exercises meet schema requirements
+    )
+        .then((workouts) => {
+            res.json(workouts);
+        })
+        // error handling
+        .catch((err) => {
+            res.json(err);
+        });
+});
 // route for getting workouts in range -- GET
 // route for deleting workouts -- DELETE
